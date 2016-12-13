fe4728692bc893938001d4bb38bf16456e159fde
linh <linh@linhdomain.com>
2016-12-13 11:11:11 +0700

    add root view for window.

--- FourSquare/Controllers/AppDelegate/AppDelegate.swift
+++ FourSquare/Controllers/AppDelegate/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions
                      launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = UIColor.white
+        let vc = UIViewController()
+        window?.rootViewController = vc
+        
         return true
     }
 }
