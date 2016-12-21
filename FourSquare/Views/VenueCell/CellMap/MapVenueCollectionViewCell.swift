68a0cd090a4555088c159f28cff8e2e8d74bd10b
Linh Vo D <duylinh158@gmail.com>
2016-12-21 17:58:15 +0700

    ok

---
+++ FourSquare/Views/VenueCell/CellMap/MapVenueCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MapVenueCollectionViewCell.swift
+//  FourSquare
+//
+//  Created by Duy Linh on 12/21/16.
+//  Copyright © 2016 Duy Linh. All rights reserved.
+//
+
+import UIKit
+
+class MapVenueCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
