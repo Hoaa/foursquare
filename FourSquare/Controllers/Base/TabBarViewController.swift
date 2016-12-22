ca6f3cc0581812472b42f6aa1a875fdf143bebbd
nmint8m <nmint8m@nmint8m.local>
2016-12-22 20:01:30 +0700

    Fix and delete some unused files

--- FourSquare/Controllers/Base/TabBarViewController.swift
+++ FourSquare/Controllers/Base/TabBarViewController.swift
@@ -22,44 +22,31 @@ class TabBarViewController: UITabBarController {
 
     // MARK: - Private Functions
     private func configItemViewController() {
-        let listVC = ListViewController(nibName: "ListViewController", bundle: nil)
-        let naviListVC = UINavigationController(rootViewController: listVC)
-        naviListVC.navigationBar.isTranslucent = false
-        // - nmint8m begin -------------------------------------------------------
-        let listsVC = ListsViewController(nibName: "ListsViewController", bundle: nil)
-        let naviListsVC = UINavigationController(rootViewController: listsVC)
-        naviListsVC.navigationBar.isTranslucent = false
-        // - nmint8m end ---------------------------------------------------------
         let searchVC = SearchViewController(nibName: "SearchViewController", bundle: nil)
         let naviSearchVC = UINavigationController(rootViewController: searchVC)
         naviSearchVC.navigationBar.isTranslucent = false
+        let listsVC = ListsViewController(nibName: "ListsViewController", bundle: nil)
+        let naviListsVC = UINavigationController(rootViewController: listsVC)
+        naviListsVC.navigationBar.isTranslucent = false
         let historyVC = HistoryViewController(nibName: "HistoryViewController", bundle: nil)
         let naviHistoryVC = UINavigationController(rootViewController: historyVC)
         naviHistoryVC.navigationBar.isTranslucent = false
         let profileVC = ProfileViewController(nibName: "ProfileViewController", bundle: nil)
         let naviProfileVC = UINavigationController(rootViewController: profileVC)
         naviProfileVC.navigationBar.isTranslucent = false
-        self.viewControllers = [naviListVC,
+        self.viewControllers = [naviSearchVC,
                                 naviListsVC,
-                                naviSearchVC,
                                 naviHistoryVC,
                                 naviProfileVC]
-        let itemList = UITabBarItem(title: "List", image: #imageLiteral(resourceName: "Lists"), tag: 0)
-        // - nmint8m begin -------------------------------------------------------
         let itemLists = UITabBarItem(title: "List", image: #imageLiteral(resourceName: "Lists"), tag: 4)
-        // - nmint8m end ---------------------------------------------------------
         let itemSearch = UITabBarItem(title: "Search", image: #imageLiteral(resourceName: "Search"), tag: 1)
         let itemHistory = UITabBarItem(title: "History", image: #imageLiteral(resourceName: "History"), tag: 2)
         let itemProfile = UITabBarItem(title: "Me", image: #imageLiteral(resourceName: "Profile"), tag: 3)
-        listVC.tabBarItem = itemList
-        // - nmint8m begin -------------------------------------------------------
         listsVC.tabBarItem = itemLists
-        // - nmint8m end ---------------------------------------------------------
         searchVC.tabBarItem = itemSearch
         historyVC.tabBarItem = itemHistory
         profileVC.tabBarItem = itemProfile
         tabBar.isTranslucent = false
         tabBar.tintColor = UIColor(red: 0, green: 153/255, blue: 255/255, alpha: 1)
-
     }
 }
